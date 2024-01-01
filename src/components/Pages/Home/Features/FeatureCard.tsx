1309535c8a27eb530bfb796b7b1173732f4a357f
git-sujon <git.sujon@gmail.com>
2024-01-01 16:20:51 +0600

    Feature section complete

--- src/components/Pages/Home/Features/FeatureCard.tsx
+++ src/components/Pages/Home/Features/FeatureCard.tsx
@@ -1,9 +1,25 @@
-const FeatureCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { Button, Card, CardBody, CardHeader, Tooltip } from "@nextui-org/react";
+
+const FeatureCard = ({ feature }: { feature: any }) => {
+  const { title, description, icon } = feature;
+  return (
+    <div>
+      <Card className="bg-white shadow-lg p-2  transition-transform transform hover:scale-105">
+        <CardHeader className="flex items-center justify-center text-secondary">
+          {icon}
+        </CardHeader>
+        <CardBody>
+          <h3 className="text-xl font-bold mb-2 text-center text-secondary">{title}</h3>
+          {/* <p className="text-gray-600">{description}</p> */}
+          <Tooltip showArrow={true} content={description} classNames={{
+            content:"max-w-sm"
+          }} >
+            <Button variant="light" color="primary" className="text-xs hover:bg-primary">Learn More...</Button>
+          </Tooltip>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default FeatureCard;
--- src/constants/featuresData.tsx
+++ src/constants/featuresData.tsx
@@ -1,4 +1,4 @@
-import { MdOutlineDevices, MdSecurity, MdAttachMoney, MdTimeline,  MdReceipt, MdCreditCard, MdPeople, MdBackup } from "react-icons/md";
+import { MdOutlineDevices, MdSecurity, MdAttachMoney, MdTimeline,  MdReceipt, MdCreditCard, MdPeople, MdBackup, MdMonetizationOn } from "react-icons/md";
 
 export const featuresData = [
   {
@@ -6,48 +6,56 @@ export const featuresData = [
     title: "Online Payment",
     description:
       "Streamline your transactions with our online payment platform. Effortlessly handle bill payments, fund transfers, and purchases securely. Enjoy the simplicity of digital transactions, ensuring convenience and peace of mind. Welcome to a hassle-free online payment experience with us.",
-    icon: <MdOutlineDevices />,
+    icon: <MdOutlineDevices className="size-10" />,
   },
   {
     id: 2,
     title: "Enhanced Security",
     description:
       "Rest easy with our advanced security measures. We prioritize the protection of your financial data, employing robust encryption and cutting-edge security protocols to ensure a secure and worry-free banking experience.",
-    icon: <MdSecurity />,
+    icon: <MdSecurity className="size-10" />,
   },
   {
     id: 3,
     title: "Digital Wallet",
     description:
       "Carry your finances in your pocket with our digital wallet feature. Seamlessly manage your cards, make contactless payments, and enjoy the convenience of a secure and efficient digital financial companion.",
-    icon: <MdCreditCard />,
+    icon: <MdCreditCard className="size-10" />,
   },
   {
     id: 4,
     title: "Financial Planning",
     description:
       "Empower your financial journey with our comprehensive financial planning tools. Track your expenses, set savings goals, and gain insights into your financial health with user-friendly features designed to help you achieve your financial objectives.",
-    icon: <MdTimeline />,
+    icon: <MdTimeline className="size-10" />,
   },
   {
     id: 5,
     title: "Automatic Backups",
     description:
       "Rest assured with automatic backups of your financial data. Our digital bank prioritizes data integrity, regularly creating secure backups to safeguard your information. Experience peace of mind knowing that your financial records are protected and easily recoverable.",
-    icon: <MdBackup />,
+    icon: <MdBackup className="size-10" />,
   },
   {
     id: 6,
     title: "E-Statements",
     description:
       "Go paperless with our electronic statements. Access your account statements conveniently through our digital platform, contributing to environmental sustainability and reducing clutter in your mailbox.",
-    icon: <MdReceipt />,
+    icon: <MdReceipt className="size-10" />,
   },
   {
     id: 7,
     title: "Instant Customer Support",
     description:
       "Experience prompt assistance with our instant customer support. Whether you have inquiries or need assistance, our dedicated support team is just a click away, ensuring a seamless and responsive customer service experience.",
-    icon: <MdPeople />,
+    icon: <MdPeople className="size-10" />,
+  },
+
+  {
+    id: 8,
+    title: "Investment Opportunities",
+    description:
+      "Explore diverse investment opportunities with our digital bank. From stocks and mutual funds to digital assets, our platform provides you with access to a range of investment options, empowering you to grow your wealth and achieve your financial goals.",
+    icon: <MdMonetizationOn className="size-10"  />,
   },
 ];
