2586332d12b0ad95e05b60369f2dbbc18e152630
git-sujon <git.sujon@gmail.com>
2024-01-06 19:26:08 +0600

    message component complete

--- src/components/Layout/Header/DashboardHeader.tsx
+++ src/components/Layout/Header/DashboardHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdOutlineMailOutline } from "react-icons/md";
 import {
   Navbar,
   NavbarBrand,
@@ -17,6 +17,8 @@ import {
   DropdownMenu,
   DropdownItem,
   Badge,
+  Card,
+  ButtonGroup,
 } from "@nextui-org/react";
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { FaRegEnvelope } from "react-icons/fa";
@@ -24,41 +26,21 @@ import Image from "next/image";
 import { ThemeSwitcher } from "@/components/Utility/ThemeSwitcher";
 import notifications from "@/constants/notifications";
 import { formatTimestamp } from "@/utils/formatTimeStamp";
+import messages from "@/constants/message";
 const DashboardHeader = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const [isInvisible, setIsInvisible] = React.useState(false);
   const menuItems = [
     {
       id: "1",
       title: "Home",
       href: "/",
     },
-    {
-      id: "2",
-      title: "Invest",
-      href: "/invest",
-    },
-
-    {
-      id: "3",
-      title: "Loan",
-      href: "/loan",
-    },
-    {
-      id: "4",
-      title: "Payments",
-      href: "/payments",
-    },
-    {
-      id: "5",
-      title: "My Account",
-      href: "/login",
-    },
   ];
 
-  const notificationCount = notifications?.length
+  const messageCount = messages.length; // Calculate the number of messages
+  const isInvisible = messageCount === 0; // Check if there are no messages
 
-
+  const notificationCount = notifications?.length;
 
   return (
     <Navbar
@@ -99,27 +81,10 @@ const DashboardHeader = () => {
           />
         </NavbarBrand>
       </NavbarContent>
+    
 
-      {/* Menus */}
-
-      <NavbarContent className="hidden sm:flex gap-4 " justify="start">
-        {menuItems.map((item) => (
-          <NavbarItem key={item.id}>
-            {item.href === "/login" ? (
-              ""
-            ) : (
-              <Link
-                className="text-slate-800 dark:text-white hover:text-primary font-semibold"
-                href={item.href}
-              >
-                {item.title}
-              </Link>
-            )}
-          </NavbarItem>
-        ))}
-      </NavbarContent>
-
-      <NavbarContent justify="end">
+      <NavbarContent justify="end" className="m-0 p-0">
+      <ButtonGroup size="lg">
         <NavbarItem>
           <ThemeSwitcher />
         </NavbarItem>
@@ -127,25 +92,62 @@ const DashboardHeader = () => {
         <NavbarItem>
           <Dropdown placement="bottom-end" backdrop="blur">
             <DropdownTrigger>
-              <Badge
-                color="danger"
-                content={notificationCount}
-                isInvisible={isInvisible}
-                shape="circle"
-              >
-                <MdOutlineNotificationsActive className="text-3xl cursor-pointer" />
-              </Badge>
+              <Button  size="md" variant="light" isIconOnly  >
+                <Badge
+                  color="danger"
+                  content={messageCount}
+                  isInvisible={isInvisible}
+                  shape="circle"
+                >
+                  <MdOutlineMailOutline className="text-3xl" />
+                </Badge>
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Message Actions" variant="flat">
+              {messages.map((message) => (
+                <DropdownItem
+                  key={message.id}
+                  className={``}
+                  variant="shadow"
+                  color="primary"
+                  showDivider={true}
+                >
+                  <div className="message-content hover:text-white p-2">
+                    <p className="message-message">{message.message}</p>
+                    <p className="message-timestamp">
+                      {formatTimestamp(message.timestamp)}
+                    </p>
+                  </div>
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarItem>
+
+        <NavbarItem>
+          <Dropdown placement="bottom-end" backdrop="blur">
+            <DropdownTrigger>
+              <Button className="" size="md" variant="light" isIconOnly >
+                <Badge
+                  color="danger"
+                  content={notificationCount}
+                  isInvisible={isInvisible}
+                  shape="circle"
+                >
+                  <MdOutlineNotificationsActive className="text-3xl " />
+                </Badge>
+              </Button>
             </DropdownTrigger>
             <DropdownMenu aria-label="Profile Actions" variant="flat">
               {notifications.map((notification) => (
                 <DropdownItem
                   key={notification.id}
-                  className={`notification-item ${notification.type}`}
+                  className={` `}
                   variant="shadow"
                   color="primary"
-                
+                  showDivider={true}
                 >
-                  <div className="notification-content hover:text-white border border-slate-300 rounded-md hover:border-0 p-2">
+                  <div className="notification-content hover:text-white  p-2">
                     <p className="notification-message">
                       {notification.message}
                     </p>
@@ -158,7 +160,7 @@ const DashboardHeader = () => {
             </DropdownMenu>
           </Dropdown>
         </NavbarItem>
-{/* 
+
         <NavbarItem>
           <Dropdown placement="bottom-end">
             <DropdownTrigger>
@@ -183,15 +185,15 @@ const DashboardHeader = () => {
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
-        </NavbarItem> */}
-
+        </NavbarItem>
+        </ButtonGroup>
       </NavbarContent>
 
-      <NavbarMenu>
+      {/* <NavbarMenu>
         {menuItems.map((item) => (
           <NavbarMenuItem key={item.id}></NavbarMenuItem>
         ))}
-      </NavbarMenu>
+      </NavbarMenu> */}
     </Navbar>
   );
 };
