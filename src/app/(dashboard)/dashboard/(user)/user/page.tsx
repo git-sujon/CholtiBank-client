586ca90cf7014ebeb54ad53569f951429c64610e
git-sujon <git.sujon@gmail.com>
2024-01-09 00:05:49 +0600

    transactionApi and adminApi created

--- src/app/(dashboard)/dashboard/(user)/user/page.tsx
+++ src/app/(dashboard)/dashboard/(user)/user/page.tsx
@@ -3,7 +3,7 @@ import ProfileCard from "@/components/Dashboard/LandingPage/ProfileCard";
 const UserDashboard = () => {
   return (
     <div className="min-h-svh p-4 lg:p-6">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
         <BalanceCardsHolder />
 
         <ProfileCard />
--- src/components/Dashboard/LandingPage/ProfileCard.tsx
+++ src/components/Dashboard/LandingPage/ProfileCard.tsx
@@ -1,9 +1,13 @@
+import { Avatar, Card } from "@nextui-org/react";
+
 const ProfileCard = () => {
-    return (
-        <div>
-            p pppppppppppp
-        </div>
-    );
+  return (
+    <Card className=" bg-yellow-50 dark:bg-slate-900 dark:text-white shadow-md w-full  border border-primary hover:border-secondary   transition-transform transform hover:scale-105 flex items-center ">
+      <div className="mt-8">
+        <Avatar src="/user.jpg" className="w-20 h-20 text-large" />
+      </div>
+    </Card>
+  );
 };
 
 export default ProfileCard;
