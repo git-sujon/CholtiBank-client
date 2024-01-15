8b71093f235e7cb1501d85fbd56c0af8c8dce945
git-sujon <git.sujon@gmail.com>
2024-01-16 00:13:42 +0600

    TransactionDetails updated

--- src/components/Transaction/TransactionDetails.tsx
+++ src/components/Transaction/TransactionDetails.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { formatTimestamp } from "@/utils/formatTimeStamp";
+import { FaCreditCard, FaMobileAlt, FaWallet } from "react-icons/fa";
 
-const TransactionDetails = ({ transaction }) => {
-
+const TransactionDetails = ({ transaction }: { transaction: any }) => {
   const renderDepositDetails = () => {
     const {
       amount,
@@ -10,16 +10,19 @@ const TransactionDetails = ({ transaction }) => {
       bankAccountNo,
       creditCardName,
       creditCardNumber,
-      createdAt
+      createdAt,
     } = transaction.deposit;
 
     return (
-      <div className="mt-4"> 
-        <h3 className="text-lg font-medium mb-2">Deposit Details</h3>
-        
+      <div className="mt-4">
+        <h3 className="flex items-center text-green-500">
+          <FaCreditCard className="mr-2" />
+          Deposit Details
+        </h3>
+
         <div className="grid grid-cols-2 gap-4">
           <div>
-            <p className="font-semibold">Amount:</p> 
+            <p className="font-semibold">Amount:</p>
             <p className="text-gray-600">{amount}</p>
           </div>
 
@@ -27,30 +30,36 @@ const TransactionDetails = ({ transaction }) => {
             <p className="font-semibold">Deposit Source:</p>
             <p className="text-gray-600">{depositSource}</p>
           </div>
+          {bankName && (
+            <div>
+              <p className="font-semibold">Bank Name:</p>
+              <p className="text-gray-600">{bankName}</p>
+            </div>
+          )}
+          {bankAccountNo && (
+            <div>
+              <p className="font-semibold">Bank Account No:</p>
+              <p className="text-gray-600">{bankAccountNo}</p>
+            </div>
+          )}
+
+          {creditCardName && (
+            <div>
+              <p className="font-semibold">Credit Card Name:</p>
+              <p className="text-gray-600">{creditCardName}</p>
+            </div>
+          )}
+
+          {creditCardNumber && (
+            <div>
+              <p className="font-semibold">Credit Card Number:</p>
+              <p className="text-gray-600">{creditCardNumber}</p>
+            </div>
+          )}
 
           <div>
-            <p className="font-semibold">Bank Name:</p>
-            <p className="text-gray-600">{bankName}</p>
-          </div>
-
-          <div>
-            <p className="font-semibold">Bank Account No:</p>
-            <p className="text-gray-600">{bankAccountNo}</p>
-          </div>
-
-          <div>
-            <p className="font-semibold">Credit Card Name:</p>
-            <p className="text-gray-600">{creditCardName}</p>
-          </div>
-        
-          <div>
-            <p className="font-semibold">Credit Card Number:</p>
-            <p className="text-gray-600">{creditCardNumber}</p>  
-          </div>
-
-          <div> 
-            <p className="font-semibold">Created At:</p>
-            <p className="text-gray-600">{createdAt}</p>
+            <p className="font-semibold">Date & Time:</p>
+            <p className="text-gray-600 text-sm  mt-2">{formatTimestamp(createdAt)}</p>
           </div>
         </div>
       </div>
@@ -58,44 +67,44 @@ const TransactionDetails = ({ transaction }) => {
   };
 
   const renderWithdrawalDetails = () => {
-    const {
-      amount, 
-      withdrawSource,
-      atmId,
-      agentId,
-      createdAt
-    } = transaction.withdrawal;
+    const { amount, withdrawSource, atmId, agentId, createdAt } =
+      transaction.withdrawal;
 
     return (
       <div className="mt-4">
-        <h3 className="text-lg font-medium mb-2">
-          Withdrawal Details
+        <h3 className="flex items-center text-yellow-500">
+          <FaWallet className="mr-2" />
+          Withdrawal Details 
         </h3>
-        
+
         <div className="grid grid-cols-2 gap-4">
           <div>
             <p className="font-semibold">Amount:</p>
             <p className="text-gray-600">{amount}</p>
           </div>
 
-          <div> 
+          <div>
             <p className="font-semibold">Withdraw Source:</p>
             <p className="text-gray-600">{withdrawSource}</p>
           </div>
 
-          <div>
-            <p className="font-semibold">ATM ID:</p>
-            <p className="text-gray-600">{atmId}</p>  
-          </div>
+          {atmId && (
+            <div>
+              <p className="font-semibold">ATM ID:</p>
+              <p className="text-gray-600">{atmId}</p>
+            </div>
+          )}
+
+          {agentId && (
+            <div>
+              <p className="font-semibold">Agent ID:</p>
+              <p className="text-gray-600">{agentId}</p>
+            </div>
+          )}
 
           <div>
-            <p className="font-semibold">Agent ID:</p>
-            <p className="text-gray-600">{agentId}</p>
-          </div>
-
-          <div>
-            <p className="font-semibold">Created At:</p>
-            <p className="text-gray-600">{createdAt}</p>
+            <p className="font-semibold">Date & Time:</p>
+            <p className="text-gray-600 text-sm  mt-2">{formatTimestamp(createdAt)}</p>
           </div>
         </div>
       </div>
@@ -103,20 +112,16 @@ const TransactionDetails = ({ transaction }) => {
   };
 
   const renderRechargeDetails = () => {
-    const {
-      amount,
-      phoneNumber,
-      mobileOperators,
-      simType, 
-      createdAt
-    } = transaction.mobileRecharge;
+    const { amount, phoneNumber, mobileOperators, simType, createdAt } =
+      transaction.mobileRecharge;
 
     return (
       <div className="mt-4">
-        <h3 className="text-lg font-medium mb-2">
-          Mobile Recharge Details  
+       <h3 className="flex items-center text-blue-500">
+          <FaMobileAlt className="mr-2" />
+          Recharge Details
         </h3>
-        
+
         <div className="grid grid-cols-2 gap-4">
           <div>
             <p className="font-semibold">Amount:</p>
@@ -134,36 +139,32 @@ const TransactionDetails = ({ transaction }) => {
           </div>
 
           <div>
-            <p className="font-semibold">SIM Type:</p> 
+            <p className="font-semibold">SIM Type:</p>
             <p className="text-gray-600">{simType}</p>
           </div>
-        
+
           <div>
-            <p className="font-semibold">Created At:</p>
-            <p className="text-gray-600">{createdAt}</p>
+            <p className="font-semibold">Date & Time:</p>
+            <p className="text-gray-600 text-sm  mt-2">{formatTimestamp(createdAt)}</p>
           </div>
-        </div>  
+        </div>
       </div>
     );
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-6">
+    <div className="bg-white  p-6">
       {/* Transaction Summary */}
 
-      {transaction.transactionType === "Deposit" && 
-        renderDepositDetails()  
-      }
+      {transaction.transactionType === "Deposit" && renderDepositDetails()}
 
-      {transaction.transactionType === "Withdrawal" && 
-        renderWithdrawalDetails()
-      }
+      {transaction.transactionType === "Withdrawal" &&
+        renderWithdrawalDetails()}
 
       {transaction.transactionType === "Mobile_Recharge" &&
-        renderRechargeDetails()
-      }
+        renderRechargeDetails()}
     </div>
-  );  
+  );
 };
 
 export default TransactionDetails;
